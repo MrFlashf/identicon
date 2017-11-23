7fa382265f1df791b7bb0dfe3895d5496e1b0696
MrFlashf <krz.jozefowicz@gmail.com>
2017-11-23 16:28:29 +0100

    Add building pixel map

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,11 +1,61 @@
 defmodule Identicon do
+  alias Identicon.Image
+
   def main(input) do
     input
     |> hash_input
+    |> pick_color
+    |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
+  end
+
+  def build_pixel_map(%Image{grid: grid} = image) do
+    pixel_map = Enum.map grid, fn({_code, index}) ->
+      horizontal  = rem(index, 5) * 50
+      vertical    = div(index, 5) * 50
+
+      top_left    = {horizontal, vertical}
+      bottom_right= {horizontal + 50, vertical + 50}
+
+      {top_left, bottom_right}
+    end
+
+    %Image{image | pixel_map: pixel_map}
+  end
+
+  def filter_odd_squares(%Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn({code, _index}) ->
+      rem(code, 2) == 0
+    end
+
+    %Image{image | grid: grid}
+  end
+
+  def build_grid(%Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Image{image | grid: grid}
+  end
+
+  def mirror_row([first, second | _tail] = row) do
+    row ++ [second, first]
+  end
+
+  def pick_color(%Image{hex: [r, g, b | _tail]} = image) do
+    %Image{image | color: {r, g, b}}
   end
 
   def hash_input(input) do
-    :crypto.hash(:md5, input)
+    hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
+
+    %Image{hex: hex}
   end
 end
+
